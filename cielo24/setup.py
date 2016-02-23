f399603178704ee1269170c3a9ff064e45c7c733
Vietor Davis <vietor.davis@cielo24.com>
2016-02-23 15:47:55 -0800

    Enable 2to3 conversion

--- cielo24/setup.py
+++ cielo24/setup.py
@@ -10,5 +10,6 @@ setup(name='cielo24',
       author_email='support@cielo24.com',
       url='http://www.cielo24.com',
       packages=['cielo24'],
-      install_requires=['enum34==1.0.4']
+      install_requires=['enum34==1.0.4'],
+      use_2to3=True,
       )
